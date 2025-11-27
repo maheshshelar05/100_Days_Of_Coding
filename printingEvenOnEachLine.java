424ec902948234417506e240e6455ceecdefee4f
Mahesh Shelar <mshelar334@gmail.com>
2025-11-27 11:02:50 +0530

    Printing even number on each line

---
+++ printingEvenOnEachLine.java
@@ -0,0 +1,11 @@
+class printingEvenOnEachLine {
+public static void main(String[] args) {
+   
+   for(int i=0; i<=100; i++) {
+    if(i%2==0)
+       System.out.println(i);
+    else
+        continue;
+   }
+}
+}
