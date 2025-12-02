7dd3b7fe859b0fd4a3c5fe03a9fb614b2d3a8955
Mahesh Shelar <mshelar334@gmail.com>
2025-12-02 09:57:28 +0530

    printing prime numbers between 1 to 200.

---
+++ primeNumber.java
@@ -0,0 +1,19 @@
+class primeNumber
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=200;i++)
+        {
+            int count=0;
+            for(int j=1;j<=i;j++)
+            {
+               if(i % j==0)
+               count++;
+            }
+        if(count==2)
+        System.out.println(i);
+        }
+
+        
+    }
+}
