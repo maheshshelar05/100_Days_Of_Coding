aa93962c5bed63b7e7994af6ef1bcaacbf4d7bfe
Mahesh Shelar <mshelar334@gmail.com>
2025-12-14 19:10:37 +0530

    Implement logic to print given string reverse without using built in reverse function .

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class ReverseString
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String :");
+        String str = sc.nextLine();
+
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            System.out.print(str.charAt(i));
+        }
+    }
+}
