712cc25e9b90a7e202a6dd4182871ca3d385bf6f
Mahesh Shelar <mshelar334@gmail.com>
2025-12-24 20:58:44 +0530

    Add program to insert text into StringBuffer .

---
+++ StringBufferDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class StringBufferDemo
+{
+     public static void main(String args[]) {
+
+        
+
+        StringBuffer sb = new StringBuffer("I java");
+        sb.insert(2,"love ");
+
+
+        System.out.println(sb);
+     }
+
+}
