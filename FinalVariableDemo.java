38c47e31d774420668ac55ec55cdeab23c1ccda3
Mahesh Shelar <mshelar334@gmail.com>
2026-01-14 22:37:40 +0530

    Add Program to demo final variable .

---
+++ FinalVariableDemo.java
@@ -0,0 +1,10 @@
+class FinalVariableDemo {
+    public static void main(String args[]) {
+
+        final int MAX = 100;
+
+        System.out.println("Value of MAX: " + MAX);
+
+        // MAX = 200;   cannot assign a value to final variable
+    }
+}
